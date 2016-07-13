d550f3ab88af235a48c880da71f693d58b55d97f
Seanna94 <1403025820@qq.com>
2016-07-13 10:50:29 +0800

    优化了代码，添加了权限管理

---
+++ src/main/java/com/kaishengit/mapper/RoleMapper.java
@@ -0,0 +1,7 @@
+package com.kaishengit.mapper;
+
+import com.kaishengit.pojo.Role;
+
+public interface RoleMapper {
+    Role findById(Integer roleId);
+}
---
+++ src/main/java/com/kaishengit/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.kaishengit.mapper;
+
+import com.kaishengit.pojo.User;
+
+public interface UserMapper {
+    User findByUsername(String username);
+}
